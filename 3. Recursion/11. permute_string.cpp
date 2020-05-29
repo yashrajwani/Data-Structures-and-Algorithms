743a61ceaee88a8aedf5ee8ac70ba6fa7d53de00
yashrajwani <yashrajwani9@gmail.com>
2020-05-29 13:00:42 +0530

    Completed recursion quiz

---
+++ 3. Recursion/11. permute_string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void permute(string &str, int l = 0)
+{
+    r = str.length() - 1;
+    if(l == r)
+    {
+        cout << str << " ";
+        return;
+    }
+    
+    for (int i = l, i <= r, ++i)
+    {
+        swap(str[i], str[l]);
+        permute(str, l + 1);
+        swap(str[i], str[l]);
+    }
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    permute(str);
+    return 0;
+}
