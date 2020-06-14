dedc04ca5dda144fb0bb307b2b1848cf28d24a09
yashrajwani <yashrajwani9@gmail.com>
2020-06-14 15:26:28 +0530

    Completed chapter arrays

--- 4. Arrays/19. index_diff_prob.cpp
+++ 4. Arrays/19. index_diff_prob.cpp
@@ -1,23 +1,25 @@
 //Given an array A[] of N positive integers. 
 //The task is to find the maximum of j - i subjected to the constraint of A[i] <= A[j].
-
+#include <iostream>
+using namespace std;
 
 int maxIndexDiff(int arr[], int n) 
 { 
     
     // Your code here
     int max_diff, i, j;
-    int* lmin = new int[n];
-    int* rmax = new int[n];
+    int *lmin = new int[n];
+    int *rmax = new int[n];
     
     lmin[0] = arr[0];
     for (int i = 1; i < n; ++i)
         lmin[i] = min(lmin[i-1], arr[i]);
         
     rmax[n-1] = arr[n-1];
-    for (int i = n-1; i >= 0; --i)
+    for (int i = n-2; i >= 0; --i)
         rmax[i] = max(rmax[i+1], arr[i]);
         
+    //
     i = 0; j = 0; max_diff = -1;
     while ((i < n) && (j < n))
     {
@@ -33,7 +35,8 @@ int maxIndexDiff(int arr[], int n)
 
 int main()
 {
-    int arr[] = {34,8,10,3,2,80,30,33,1}
-    int n = sizeof(arr)/ sizeof(arr[0]);
+    int arr[] = {34,8,10,3,2,80,30,33,1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << maxIndexDiff(arr, n);
     return 0;
 }
